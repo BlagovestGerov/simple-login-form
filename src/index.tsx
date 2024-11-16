bcbf03f76055230a4273485954972c053db260b3
blaje <blaje@abv.bg>
2024-11-16 12:37:44 +0200

    remove env restrictions

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import Root from "./AuthProvider";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
 
   const { worker } = await import("./api/browser");
 
